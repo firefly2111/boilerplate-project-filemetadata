f825d880cc848df07529bc25c0345e81b20f21a2
Nikola Milicevic <nikolamilicevic99@yahoo.com>
2019-04-03 22:21:16 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,22 +1,26 @@
 'use strict';
-
 var express = require('express');
 var cors = require('cors');
-
+var bodyParser = require("body-parser");
 // require and use "multer"...
-
+var multer = require("multer");
+var upload = multer({dest: "./public"});
 var app = express();
-
 app.use(cors());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 app.use('/public', express.static(process.cwd() + '/public'));
-
-app.get('/', function (req, res) {
-     res.sendFile(process.cwd() + '/views/index.html');
-  });
-
-app.get('/hello', function(req, res){
-  res.json({greetings: "Hello, API"});
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
 });
+
+app.route("/api/fileanalyse").post(
+  upload.single("upfile"), (req, res) => {
+    var fileSize = req.file.size + " bytes";
+    res.json({name: req.file.originalname, size: fileSize});
+  }
+)
+
 
 app.listen(process.env.PORT || 3000, function () {
   console.log('Node.js listening ...');
